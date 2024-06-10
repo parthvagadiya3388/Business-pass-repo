dacce50a8564c9535a18f89fe690360d7f45a037
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-06-10 17:42:47 +0530

    remove the route path

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ import Loginpage from "./components/login-page";
 
 function App() {
   return (
-    <>  
+    <>
   
       <Router>
 
           <Routes>
-            <Route path="/login" element={<Loginpage />} />
+            <Route path="/" element={<Loginpage />} />
             <Route path="/header" element={<Header />} />
             <Route path="/userpage" element={<Userpage />} />
             <Route path="/createpage" element={<Createpage />} />
