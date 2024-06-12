4eaca54414054c7af1cc828e9502d44ab9bdd8df
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-06-12 17:18:59 +0530

    Created formvalue interface, show error below all inputfield using formic and yup.client side show error, error message rendered from file in schema.

--- src/components/create-page.tsx
+++ src/components/create-page.tsx
@@ -1,63 +1,85 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Header from "./header";
 import { AiTwotoneDashboard } from "react-icons/ai";
 import { PiUsersThreeBold } from "react-icons/pi";
 import { TbUsers } from "react-icons/tb";
 import { Card, Col, Container, InputGroup, Row, Form, Button } from "react-bootstrap";
 import { BsBagCheck } from "react-icons/bs";
+import { API_URL } from "../config";
+import { useFormik, FormikHelpers } from "formik";
+import schema from "../validation/validation-schema";
+
+interface FormValues {
+  name: string;
+  email: string;
+  country: string;
+  phone_number: string;
+  key: string;
+  user_type: string;
+  password: string;
+  confirm_password: string;
+}
+
+const initialValues: FormValues = {
+  name: "",
+  email: "",
+  country: "",
+  phone_number: "",
+  key: "",
+  user_type: "",
+  password: "",
+  confirm_password: "",
+};
 
 export default function Createpage() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [country, setCountry] = useState('');
-  const [phone_number, setPhone_number] = useState('');
-  const [key, setKey] = useState('');
-  const [user_type, setUser_type] = useState('');
+  const [error, setErrors] = useState({} as Record<string, string>);
+  const navigate = useNavigate();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    const userData = { name, email, country, phone_number, key, user_type };
+console.log("seterrors-----------------------",error)
 
-    try {
-      const token = localStorage.getItem('token');
-      const response = await fetch('http://192.168.1.17:3000/api/users/', {
-
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
+  const formik = useFormik<FormValues>({
+    initialValues,
+    validationSchema: schema,
+    onSubmit: async (values, { resetForm }: FormikHelpers<FormValues>) => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await fetch(`${API_URL}/users/`, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
           },
-          body: JSON.stringify(userData),
-          });
+          body: JSON.stringify(values),
+        });
 
-        console.log("create responce--------------",response);
+        if (response.status === 400) {
+          const errorData = await response.json();
+          setErrors(errorData.errors);
+          return;
+        }
 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+        // const data = await response.json();
+        resetForm();
+        navigate('/userpage');
+      } catch (error) {
+        console.error('Error:', error);
       }
-
-      const data = await response.json();
-      console.log(data);
-      
-    } catch (error) {
-      console.error('Error:', error);
-
     }
-  };
+  });
 
   return (
     <>
-      <Header />
+      <Header />  
       <Container fluid>
         <Row>
-          <Col md={3} className="">
-            <div className="Profile_Side_bar border_radias">
+          <Col md={3}>
+            <div className="Profile_Side_bar border_radius">
               <ul className="nav flex-column">
                 <li className="nav-item">
                   <Link className="nav-link btn btn-primary radius text-start text-dark border_radius" to="#">
-                    <AiTwotoneDashboard className="" /> Dashboard
+                    <AiTwotoneDashboard /> Dashboard
                   </Link>
                 </li>
                 <li className="nav-item">
@@ -100,7 +122,7 @@ export default function Createpage() {
             <Card className="bg-light p-4 border_radius">
               <Row>
                 <p><BsBagCheck className="mb-2" /> <strong>Add user</strong></p>   
-                <Form onSubmit={handleSubmit}>
+                <Form onSubmit={formik.handleSubmit}>
                   <div className="d-flex flex-wrap">
                     <Col md={12}>
                       <Form.Group className="mb-3">
@@ -109,14 +131,14 @@ export default function Createpage() {
                         </Form.Label>
                         <InputGroup>
                           <Form.Control
-                            className="radius border_radius"
+                            className={`radius border_radius ${formik.touched.name && formik.errors.name ? 'border-danger' : ''}`}
                             type="text"
                             placeholder="Enter your name"
                             id="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
+                            {...formik.getFieldProps('name')}
                           />
                         </InputGroup>
+                        {formik.touched.name && formik.errors.name && <div className="text-danger">{formik.errors.name}</div>}
                       </Form.Group>
                     </Col>
 
@@ -127,33 +149,31 @@ export default function Createpage() {
                         </Form.Label>
                         <InputGroup>
                           <Form.Control
-                            className="radius border_radius"
-                            type="email"
+                            className={`radius border_radius ${formik.touched.email && formik.errors.email ? 'border-danger' : ''}`}
+                            type="text"
                             placeholder="Enter your email"
                             id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            {...formik.getFieldProps('email')}
                           />
                         </InputGroup>
+                        {formik.touched.email && formik.errors.email && <div className="text-danger">{formik.errors.email}</div>}
                       </Form.Group>
                     </Col>
 
                     <Col md={6}>
-                      <Form.Group className="mb-3">
-                        <Form.Label>
-                          <strong>Country</strong>
-                        </Form.Label>
-                        <InputGroup>
-                          <Form.Control
-                            className="radius border_radius"
-                            type="text"
-                            placeholder="Enter your Country"
-                            id="country"
-                            value={country}
-                            onChange={(e) => setCountry(e.target.value)}
-                          />
-                        </InputGroup>
-                      </Form.Group>
+                      <Form.Label>
+                        <strong>Country</strong>
+                      </Form.Label>
+                      <select
+                        className={`form-select radius border_radius ${formik.touched.country && formik.errors.country ? 'border-danger' : ''}`}
+                        id="country"
+                        {...formik.getFieldProps('country')}
+                      >
+                        <option value="">Select your Country</option>
+                        <option value="IN">IN</option>
+                        <option value="USA">USA</option>
+                      </select>
+                      {formik.touched.country && formik.errors.country && <div className="text-danger">{formik.errors.country}</div>}
                     </Col>
 
                     <Col md={6}>
@@ -163,14 +183,14 @@ export default function Createpage() {
                         </Form.Label>
                         <InputGroup>
                           <Form.Control
-                            className="radius border_radius"
+                            className={`radius border_radius ${formik.touched.phone_number && formik.errors.phone_number ? 'border-danger' : ''}`}
                             type="text"
                             placeholder="999XXXXXXX"
-                            id="phone"
-                            value={phone_number}
-                            onChange={(e) => setPhone_number(e.target.value)}
+                            id="phone_number"
+                            {...formik.getFieldProps('phone_number')}
                           />
                         </InputGroup>
+                        {formik.touched.phone_number && formik.errors.phone_number && <div className="text-danger">{formik.errors.phone_number}</div>}
                       </Form.Group>
                     </Col>
 
@@ -181,14 +201,14 @@ export default function Createpage() {
                         </Form.Label>
                         <InputGroup>
                           <Form.Control
-                            className="radius border_radius"
+                            className={`radius border_radius ${formik.touched.key && formik.errors.key ? 'border-danger' : ''}`}
                             type="text"
                             placeholder="0"
                             id="key"
-                            value={key}
-                            onChange={(e) => setKey(e.target.value)}
+                            {...formik.getFieldProps('key')}
                           />
                         </InputGroup>
+                        {formik.touched.key && formik.errors.key && <div className="text-danger">{formik.errors.key}</div>}
                       </Form.Group>
                     </Col>
 
@@ -197,13 +217,51 @@ export default function Createpage() {
                         <strong>User Type</strong>
                       </Form.Label>
                       <select
-                        className="form-select border_radius"
-                        value={user_type}
-                        onChange={(e) => setUser_type(e.target.value)}
+                        className={`form-select radius border_radius ${formik.touched.user_type && formik.errors.user_type ? 'border-danger' : ''}`}
+                        id="user_type"
+                        {...formik.getFieldProps('user_type')}
                       >
+                        <option value="">Select user type</option>
                         <option value="Business">Business</option>
                         <option value="Personal">Personal</option>
                       </select>
+                      {formik.touched.user_type && formik.errors.user_type && <div className="text-danger">{formik.errors.user_type}</div>}
+                    </Col>
+
+                    <Col md={6}>
+                      <Form.Group >
+                        <Form.Label>
+                          <strong>Password</strong>
+                        </Form.Label>
+                        <InputGroup>
+                          <Form.Control
+                            className={`radius border_radius ${formik.touched.password && formik.errors.password ? 'border-danger' : ''}`}
+                            type="password"
+                            placeholder="Enter your password"
+                            id="password"
+                            {...formik.getFieldProps('password')}
+                          />
+                        </InputGroup>
+                        {formik.touched.password && formik.errors.password && <div className="text-danger">{formik.errors.password}</div>}
+                      </Form.Group>
+                    </Col>
+
+                    <Col md={6}>
+                      <Form.Group >
+                        <Form.Label>
+                          <strong>Confirm Password</strong>
+                        </Form.Label>
+                        <InputGroup>
+                          <Form.Control
+                            className={`radius border_radius ${formik.touched.confirm_password && formik.errors.confirm_password ? 'border-danger' : ''}`}
+                            type="password"
+                            placeholder="Enter your password"
+                            id="confirm_password"
+                            {...formik.getFieldProps('confirm_password')}
+                          />
+                        </InputGroup>
+                        {formik.touched.confirm_password && formik.errors.confirm_password && <div className="text-danger">{formik.errors.confirm_password}</div>}
+                      </Form.Group>
                     </Col>
 
                     <div className="container text-end">
@@ -211,7 +269,6 @@ export default function Createpage() {
                         Create
                       </Button>
                     </div>
-
                   </div>
                 </Form>
               </Row>
