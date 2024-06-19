1b7be5382894b0b9d60fae619a3cd73a04682a3e
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-06-19 18:06:56 +0530

    remove the update-page path

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,34 @@ import Userpage from "./components/user-page";
 import Createpage from "./components/create-page";
 import Loginpage from "./components/login-page";
 import Welcome from "./components/welcome-page";
-import UpdatePagee from "./components/update-page";
+import ProtectedRoute from "./components/protectedRoute";
 
 function App() {
   return (
     <>
-  
       <Router>
-
-          <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route path="/login" element={<Loginpage />} />
-            <Route path="/header" element={<Header />} />
-            <Route path="/userpage" element={<Userpage />} />
-            <Route path="/createpage" element={<Createpage />} />
-            <Route path="/update-page" element={<UpdatePagee />} />
-
-          </Routes>
-
-
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Loginpage />} />
+          <Route path="/header" element={<Header />} />
+          <Route
+            path="/userpage"
+            element={
+              <ProtectedRoute>
+                <Userpage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/createpage"
+            element={
+              <ProtectedRoute>
+                <Createpage />
+              </ProtectedRoute>
+            }
+          />
+     
+        </Routes>
       </Router>
     </>
   );
