a709f993e5d5b7636bb1d5791994baafdf1f3145
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-06-24 18:00:04 +0530

    Routing name changes all the components.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/header";
-import Userpage from "./components/user-page";
-import Createpage from "./components/create-page";
-import Loginpage from "./components/login-page";
+import Loginpage from "./components/user-login";
 import Welcome from "./components/welcome-page";
 import useLoginStore from './zustandstore/loginApiStore';
-import PasswordForgot from "./components/password-forgot";
+import Userlist from "./components/user-list";
+import Adduser from "./components/add-users";
+import Personalinfo from "./components/personal-info";
 
 function App() {
   const isAuthenticated = useLoginStore(state => state.isAuthenticated);
@@ -17,13 +17,13 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/login" element={isAuthenticated ? <Navigate to="/userpage" /> : <Loginpage />} />
+        <Route path="/login" element={isAuthenticated ? <Navigate to="/userlist" /> : <Loginpage />} />
         <Route path="/header" element={<Header />} />
         {isAuthenticated ? (
           <>
-            <Route path="/userpage" element={<Userpage />} />
-            <Route path="/createpage" element={<Createpage />} />
-            <Route path="/pwdfor" element={<PasswordForgot />} />
+            <Route path="/userlist" element={<Userlist />} />
+            <Route path="/adduser" element={<Adduser />} />
+            <Route path="/personalinfo" element={<Personalinfo />} />
           </>
         ) : null}
       </Routes>
