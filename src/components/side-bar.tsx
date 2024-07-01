b6304ea72f4c0b206b224e2533c1eca772eb82ac
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-07-01 17:22:53 +0530

    usertype based login changes.

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -3,17 +3,29 @@ import { AiTwotoneDashboard } from "react-icons/ai";
 import { PiUsersThreeBold } from "react-icons/pi";
 import { TbUsers } from "react-icons/tb";
 import { Col } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
 
 export default function Sidebar() {
+
+  const [user_type, setUser_type] = useState("");
+
+  useEffect(() => {
+    const user_type = localStorage.getItem("user_type");
+    setUser_type(user_type || "");
+  }, []);
+
+
   return (
     <Col className="Profile_Side_bar border_radias p-0">
               <ul className="nav flex-column">
-                <li className="nav-item">
-                  <Link className="nav-link btn btn-primary radius text-start border_radius text_sidebar" to="#">
-                    <AiTwotoneDashboard className='' /> Dashboard
-                  </Link>
-                </li>
+              {user_type !== "Business" && (
+                  <li className="nav-item">
+                    <Link className="nav-link btn btn-primary radius text-start border_radius text_sidebar" to="#">
+                      <AiTwotoneDashboard /> Dashboard
+                    </Link>
+                  </li>
+                )}
                 <li className="nav-item">
                   <Link className="nav-link active btn btn-primary radius text-start text-white border_radius text_sidebar" to="#">
                     <PiUsersThreeBold /> Users
@@ -24,21 +36,26 @@ export default function Sidebar() {
                     <PiUsersThreeBold /> Activate accounts
                   </Link>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link btn btn-primary radius text-start border_radius text_sidebar" to="#">
-                    <TbUsers /> Explore membership
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link btn btn-primary radius text-start border_radius text_sidebar" to="#">
-                    <TbUsers /> For Workspaces
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link btn btn-primary radius text-start border_radius text_sidebar" to="#">
-                    <TbUsers /> For Communities
-                  </Link>
-                </li>
+
+                {user_type !== "Business" && (
+                  <>
+                    <li className="nav-item">
+                      <Link className="nav-link btn btn-primary radius text-start border_radius text_sidebar" to="#">
+                        <TbUsers /> Explore membership
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link btn btn-primary radius text-start border_radius text_sidebar" to="#">
+                        <TbUsers /> For Workspaces
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link btn btn-primary radius text-start border_radius text_sidebar" to="#">
+                        <TbUsers /> For Communities
+                      </Link>
+                    </li>
+                  </>
+                )}
           
               </ul>
             </Col>
