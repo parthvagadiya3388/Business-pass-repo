bce408b40ee3d6a6f9def76aa50fce546b660023
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-07-05 18:05:14 +0530

    adduserApi and userUpdateapis add in store.

--- src/zustandstore/userApisStore.tsx
+++ src/zustandstore/userApisStore.tsx
@@ -16,9 +16,10 @@ interface User {
 interface UserState {
   users: User[];
   error: string;
-  setError: (errorMessage: string) => void; 
+  setError: (errorMessage: string) => void;
   userApis: (token: string) => Promise<void>;
   deleteUserApis: (token: string, userId: number) => Promise<void>;
+  addUser: (token: string, userData: Omit<User, 'id'>) => Promise<void>;
   userUpdateApis: (token: string, userId: number, userData: Partial<User>) => Promise<void>;
   setSelectedUser: (user: User | null) => void;
   clearSelectedUser: () => void;
@@ -34,11 +35,9 @@ const useUserStore = create<UserState>((set) => ({
     try {
       const response = await axios.get(`${API_URL}/users/`, {
         headers: {
-          'Authorization': `Bearer ${token}`
+          Authorization: `Bearer ${token}`
         }
       });
-
-      // console.log("--------userllist----------", response.data.message);
 
       if (response.data && Array.isArray(response.data.data)) {
         set({ users: response.data.data, error: '' });
@@ -62,32 +61,55 @@ const useUserStore = create<UserState>((set) => ({
       set({ error: 'Error deleting user' });
     }
   },
-  
+
+  addUser: async (token, userData) => {
+    try {
+      const response = await axios.post(`${API_URL}/users/`, userData, {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+      console.log("add user dataa--------------error" ,response);
+
+      if (response.status === 201) {
+        set((state) => ({
+          users: [...state.users, response.data]
+        }));
+      } else {
+        set({ error: 'Error adding user: Invalid response from server' });
+      }
+    } catch (error: any) {
+      if (error.response && error.response.data === 400 ) {
+         console.log("error add time----",error.response.data.errors);
+          set({error: error.response.data.errors});
+      } else {
+        set({ error: 'Error adding user' });
+      }
+    } 
+  },
+
   userUpdateApis: async (token, userId, userData) => {
     try {
       const response = await axios.patch(`${API_URL}/users/${userId}/`, userData, {
         headers: { Authorization: `Bearer ${token}` }
       });
-
-      console.log("updateApis response:", response);
+      console.log('user update response:--------', response);
 
       if (response.status === 200) {
-        console.log("responce-200********------",response);
         set((state) => ({
           users: state.users.map(user => user.id === userId ? { ...user, ...userData } : user)
         }));
-      } else { 
+      } else {
         set({ error: 'Error updating user: Invalid response from server' });
       }
     } catch (error:any) {
       if (error.response && error.response.status === 400) {
-        console.log("responsce-400**********-", error.response.data);
-        set({ error: 'Error updating user: Bad request' });
+        console.log("Error update time----",error.response.data.errors);
+          set({error: error.response.data.errors});
       } else {
         set({ error: 'Error updating user' });
       }
     }
-  },
+  },  
+
 
   setSelectedUser: (user) => set({ selectedUser: user }),
   clearSelectedUser: () => set({ selectedUser: null }),
